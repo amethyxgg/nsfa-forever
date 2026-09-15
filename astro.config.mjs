58fcf80a8d755cda49db09a550677b8257aa43b0
AmethyxGG <dani.craig86@proton.me>
2026-09-15 13:31:41 +0100

    WiP changes, news as home page, theme etc

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,30 +8,6 @@ import solidJs from '@astrojs/solid-js';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://nsfa-forever.netlify.app',
     integrations: [mdx(), sitemap(), solidJs()],
-    fonts: [
-        {
-            provider: fontProviders.local(),
-            name: 'Atkinson',
-            cssVariable: '--font-atkinson',
-            fallbacks: ['sans-serif'],
-            options: {
-                variants: [
-                    {
-                        src: ['./src/assets/fonts/atkinson-regular.woff'],
-                        weight: 400,
-                        style: 'normal',
-                        display: 'swap',
-                    },
-                    {
-                        src: ['./src/assets/fonts/atkinson-bold.woff'],
-                        weight: 700,
-                        style: 'normal',
-                        display: 'swap',
-                    },
-                ],
-            },
-        },
-    ],
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Not Safe for Azeroth';
+export const SITE_TITLE = 'Not Safe for Azeroth (Forever)';
 export const SITE_DESCRIPTION = 'Guild in World of Warcraft Forever';
