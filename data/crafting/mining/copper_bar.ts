c340e2d70278530b2138fe5c303c9561cf5775f3
AmethyxGG <amethyxgg@gmail.com>
2026-09-21 17:48:10 +0100

    Squashed 'src/wow-forever-data/' changes from 8b804b2..d51e912
    d51e912 Update craft quantity
    f2a83f7 Update copper bar
    1551c68 Materials and crafting
    464e256 Fix typo, add some materials
    67786e0 Add first known raids
    ef2bb38 Update material and raids
    
    git-subtree-dir: src/wow-forever-data
    git-subtree-split: d51e912221c30c6901aaad6052766289e50c690c

---
+++ data/crafting/mining/copper_bar.ts
@@ -0,0 +1,23 @@
+import type { Material } from "@amethyxgg/wow-forever-data/Material";
+import type { CraftedItem } from "@amethyxgg/wow-forever-data/CraftedItem";
+import CopperOre from "@amethyxgg/wow-forever-data/reagents/ore_stone_gems/copper_ore";
+
+import { Rarity } from "@amethyxgg/wow-forever-data/Rarity";
+
+export const CopperBar: CraftedItem = {
+    name: "Copper Bar",
+    url: "https://www.wowhead.com/forever/item=2840/copper-bar",
+    rarity: Rarity.Common,
+    itemId: 2840,
+    spellId: 2657,
+    type: "Crafting Material",
+    materials: [
+        {
+            ...CopperOre,
+            quantity: 1
+        }
+    ],
+    craftQuantity: 1
+};
+
+export default CopperBar;
